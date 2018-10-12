6ceea0c139afbc83c8f165139804e7b94c6d6edd
Ethan-Walker <1169091731@qq.com>
2018-10-12 22:26:32 +0800

    delete duplicate

--- src/chapter1/ch1/BinarySearch.java
+++ src/chapter1/ch1/BinarySearch.java
@@ -35,30 +35,4 @@ public class BinarySearch {
         }
     }
 
-    public static int smallerCount(int key, int[] a) {
-        int l = 0, mid = 0;
-        int r = a.length - 1;
-        while (l <= r) {
-            mid = l + (r - l) / 2;
-            if (key < a[mid]) r = mid - 1;
-            else if (key > a[mid]) l = mid + 1;
-            else break;
-        }
-        if (l <= r) {
-            // 找到相等的数
-            while (mid >= 0 && a[mid] == key) mid--;
-            return mid + 1;
-        } else {
-            // 未找到相等的数, l>r
-            if (a[l] < key) {
-                while (l < a.length && a[l] < key) l++;
-                return l;
-            } else {
-                // 从 a[r] 开始找到第一个小于 key 的数
-                while (r >= 0 && a[r] > key) r++;
-                return r + 1;
-            }
-        }
-
-    }
 }
